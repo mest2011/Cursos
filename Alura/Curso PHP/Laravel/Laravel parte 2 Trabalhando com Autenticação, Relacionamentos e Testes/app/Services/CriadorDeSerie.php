1ccefc9721a72b47325a7a9364cba69a97215bcf
Miguel Eto Sobral Teixeira <mest2011mest@gmail.com>
2021-03-11 10:49:39 -0300

    Usando serviços para exclusão - finalizado

--- Alura/Curso PHP/Laravel/Laravel parte 2 Trabalhando com Autenticação, Relacionamentos e Testes/app/Services/CriadorDeSerie.php
+++ Alura/Curso PHP/Laravel/Laravel parte 2 Trabalhando com Autenticação, Relacionamentos e Testes/app/Services/CriadorDeSerie.php
@@ -3,6 +3,7 @@
 namespace App\Services;
 
 use App\Serie;
+use Illuminate\Support\Facades\DB;
 
 class CriadorDeSerie
 {
@@ -11,16 +12,28 @@ class CriadorDeSerie
         int $qtdTemporadas,
         int $qtdEpisodios
     ): Serie {
-        $serie = Serie::create(['nome' => $nomeSerie]);
-        $qtdTemporadas = $qtdTemporadas;
-        for ($i = 1; $i <= $qtdTemporadas; $i++) {
-            $temporada = $serie->temporadas()->create(['numero' => $i]);
-
-            for ($j = 1; $j <= $qtdEpisodios; $j++) {
-                $temporada->episodios()->create(['numero' => $j]);
-            }
-        }
+        $serie = new Serie();
+        DB::beginTransaction();
+            $serie = Serie::create(['nome' => $nomeSerie]);
+            $this->criarTemporada($serie, $qtdTemporadas, $qtdEpisodios);
+            $qtdTemporadas = $qtdTemporadas;
+        DB::commit();
 
         return $serie;
+    }
+
+    private function criarTemporada($serie, $qtdTemporadas, $qtdEpisodios)
+    {
+        for ($i = 1; $i <= $qtdTemporadas; $i++) {
+            $temporada = $serie->temporadas()->create(['numero' => $i]);
+            $this->criarEpsodios($temporada, $qtdEpisodios);
+        }
+    }
+
+    private function criarEpsodios($temporada, $qtdEpisodios)
+    {
+        for ($j = 1; $j <= $qtdEpisodios; $j++) {
+            $temporada->episodios()->create(['numero' => $j]);
+        }
     }
 }
