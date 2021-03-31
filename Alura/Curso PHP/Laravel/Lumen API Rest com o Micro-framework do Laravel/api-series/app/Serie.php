bc196b7e83f54114c59c6536fc4d7a41ea606d31
Miguel Eto Sobral Teixeira <mest2011mest@gmail.com>
2021-03-31 13:54:03 -0300

    O primeiro endpoint

---
+++ Alura/Curso PHP/Laravel/Lumen API Rest com o Micro-framework do Laravel/api-series/app/Serie.php
@@ -0,0 +1,14 @@
+<?php 
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Serie extends Model{
+    public $timestamp = false;
+    protected $fillable = ['nome'];
+
+    public function episodios()
+    {
+        return $this->hasMany(Episodio::class);
+    }
+}
